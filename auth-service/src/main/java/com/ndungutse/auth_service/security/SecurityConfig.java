c16f8332d1c521a6c014ccbeedcf777cdb3988fd
ericndungutse <dav.ndungutse@gmail.com>
2025-07-06 16:04:14 +0200

    feat: add login service method

--- auth-service/src/main/java/com/ndungutse/auth_service/security/SecurityConfig.java
+++ auth-service/src/main/java/com/ndungutse/auth_service/security/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.ndungutse.auth_service.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 }
